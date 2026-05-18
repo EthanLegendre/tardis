d312a91b4212f7756cc1b38810412630388b672b
lino <lino.iannacone@epitech.eu>
2026-05-18 13:22:42 +0200

    del: import forestregressor

--- tardis_dashboard.py
+++ tardis_dashboard.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 import streamlit as st
-from sklearn.ensemble import RandomForestRegressor
 
 warnings.filterwarnings("ignore")
 
